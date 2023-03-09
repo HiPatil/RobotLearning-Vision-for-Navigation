d5a33aa865de97d729a4655bd16568548b5858d2
Himanshu Patil <hipatil1998@gmail.com>
2023-03-09 09:05:08 -0500

    Reorg repo

--- imitation_learning/run.sh
+++ imitation_learning/run.sh
@@ -1,4 +1,7 @@
 # python training.py --scratch -j 16 --model clf_CE --image_size 96x96 -b 64
 
 # For multiclass
-CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --scratch --arch multiclass -j 32 --model multiclass_clf_4 --image_size 224x224 -b 384
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --scratch --arch multiclass -j 32 --model multiclass_clf_4 --image_size 224x224 -b 384
+
+# Regression
+CUDA_VISIBLE_DEVICES=0 python training_regression.py --scratch -j 32 --model regression --image_size 160x120 -b 128 --num_epochs 100
